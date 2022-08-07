8fc8914736d55eec87fb749bb116ef19a4c3d5d8
suji8073 <suzy8073@naver.com>
2022-08-08 00:31:17 +0900

    오류 수정

--- src/components/Course/FirstView.jsx
+++ src/components/Course/FirstView.jsx
@@ -8,20 +8,23 @@ const CourseListView = ({ data }) => {
   const dataLength = useState(data.length);
   const [click, setClick] = useState(false);
   return (
-    <TouchableOpacity
-      activeOpacity={0.8}
-      onPress={() => setClick(click === false ? true : false)}
-    >
-      <View style={styles.ListView}>
-        <AntDesign name="right" size={toSize(12)} color="#8F9098" />
-        <Text style={styles.mainText}>
-          Recent travel courses created{"  "}
-          <Text style={styles.numText}>{dataLength}</Text>
-        </Text>
-        <AntDesign name="ellipsis1" size={toSize(24)} color="#8F9098" />
-      </View>
-      {click === true && data.map((data, key) => <SecondView data={data} />)}
-    </TouchableOpacity>
+    <>
+      <TouchableOpacity
+        activeOpacity={0.8}
+        onPress={() => setClick(click === false ? true : false)}
+      >
+        <View style={styles.ListView}>
+          <AntDesign name="right" size={toSize(12)} color="#8F9098" />
+          <View style={styles.textView}>
+            <Text style={styles.mainText}>Recent travel courses created</Text>
+            <Text style={styles.numText}>{dataLength}</Text>
+          </View>
+          <AntDesign name="ellipsis1" size={toSize(24)} color="#8F9098" />
+        </View>
+      </TouchableOpacity>
+      {click === true &&
+        data.map((data, key) => <SecondView data={data} key={key} />)}
+    </>
   );
 };
 export default CourseListView;
@@ -43,12 +46,15 @@ const styles = StyleSheet.create({
     shadowRadius: 2,
     elevation: 3,
   },
+  textView: {
+    flex: 1,
+    flexDirection: "row",
+  },
   mainText: {
     fontSize: toSize(14),
     color: "#1F2024",
     fontWeight: "400",
     marginLeft: toSize(18),
-    flex: 1,
   },
   numText: {
     fontSize: toSize(14),
--- src/screens/Explore/SearchTravelScreen/index.js
+++ src/screens/Explore/SearchTravelScreen/index.js
@@ -11,7 +11,7 @@ import SearchView from "../../../components/SearchView";
 import RecentView from "../../../components/RecentView";
 import { AntDesign } from "@expo/vector-icons";
 
-const SearchTravelScreen = () => {  
+const SearchTravelScreen = () => {
   const navigation = useNavigation();
   const [search, setChangeSearch] = useState("");
   const SearchArray = [
@@ -38,8 +38,8 @@ const SearchTravelScreen = () => {
         {search == "" && (
           <View style={styles.RecentSearchView}>
             <Text style={styles.recentMainText}>Recent Search</Text>
-            {SearchArray.map((array) => (
-              <RecentView content={array.content} />
+            {SearchArray.map((array, key) => (
+              <RecentView content={array.content} key={key} />
             ))}
           </View>
         )}
