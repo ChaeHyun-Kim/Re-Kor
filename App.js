af99f86448dd000d833f19cf19aebba3c79a740c
Suji Chae <63655826+suji8073@users.noreply.github.com>
2022-08-12 21:10:23 +0900

    UP

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import App from "./src/App";
 import { Text, TextInput } from "react-native";
 import React from "react";
@@ -15,3 +16,25 @@ export default class extends React.Component {
     return <App />;
   }
 }
+=======
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+>>>>>>> parent of 45cf004 (update!)
